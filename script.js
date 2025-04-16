9e83736b9c3ef3e6e6cad471e4da69d44a5ea78c
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-04-16 23:25:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your JS code here. If required.
+const submitBtn = document.getElementById("submit");
+
+submitBtn.addEventListener('click',function buttonClick(){
+	
+	let bookTitle = document.getElementById("title").value.trim();
+	let bookAuthor = document.getElementById("author").value.trim();
+	let bookIsbn = document.getElementById("isbn").value.trim();	
+	
+	const tableBody = document.getElementById("book-list");
+	const bookRow = document.createElement("tr");
+	
+	const newbookTitle = document.createElement("td");
+	newbookTitle.textContent = bookTitle;
+	bookRow.appendChild(newbookTitle)
+	
+	const newBookAuthor = document.createElement("td");
+	newBookAuthor.textContent = bookAuthor;
+	bookRow.appendChild(newBookAuthor)
+	
+	const newBookIsbn = document.createElement("td");
+	newBookIsbn.textContent = bookIsbn;
+	bookRow.appendChild(newBookIsbn)
+	
+	const deleteTd = document.createElement("td")
+	const deleteBtn = document.createElement("button");
+	deleteBtn.textContent = "X";
+	deleteBtn.style.color = "red";
+	
+	//delete functionality
+	deleteBtn.addEventListener('click', ()=>{
+		bookRow.remove();
+	})
+	deleteTd.appendChild(deleteBtn);
+	bookRow.appendChild(deleteTd)
+	
+	tableBody.appendChild(bookRow);
+});
